d52fa8279b1cd83b6391cbb4ac0f7852bd9cb00d
Hovsep <hovseplalikian@gmail.com>
2019-02-10 15:27:43 -0800

    Add dungeons command

--- keystones/core/bot.py
+++ keystones/core/bot.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 
+from keystones.dungeons import dungeon_names
+
 from discord_token import DISCORD_TOKEN
 
 description = 'A bot to keep track of mythic keystones'
@@ -13,6 +15,27 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
+
+
+@bot.command(aliases=['dungeon'])
+async def dungeons():
+    """
+
+    Says the names of all dungeons and some of their
+    accepted alternative names.
+    :return: None
+    """
+    names = dungeon_names.EXAMPLE_NAMES
+    formatted_names = []
+    for name in names:
+        alternative_names = ', '.join(names[name])
+        formatted_names.append(f'{name}: {alternative_names}')
+
+    header = 'Here are the dungeons and some of their alternative names:\n'
+    dungeons = '\n'.join(formatted_names)
+
+    message = f'{header}```{dungeons}```'
+    await bot.say(message)
 
 
 def main():
