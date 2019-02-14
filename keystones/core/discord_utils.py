a0d3c402d13c96958ed806c5c899654f832f6dde
Hovsep <hovseplalikian@gmail.com>
2019-02-13 21:45:50 -0800

    Document some discord utils

--- keystones/core/discord_utils.py
+++ keystones/core/discord_utils.py
@@ -1,56 +1,70 @@
-def contains_mention_here(message):
+import discord
+
+
+def contains_mention_here(message: str):
     return '@here' in message.split()
 
 
-def contains_mention_everyone(message):
+def contains_mention_everyone(message: str):
     return '@everyone' in message.split()
 
 
-def contains_here(message):
+def contains_here(message: str):
+    """ Checks if a string contains @here or similar words """
     target = {'@here', 'here', 'online'}
     message_split = set(message.split())
     return len(target & message_split) > 0
 
 
-def contains_everyone(message):
+def contains_everyone(message: str):
+    """ Checks if a string contains @everyone or similar words """
     target = {'@everyone', 'everyone', 'all'}
     message_split = set(message.split())
     return len(target & message_split) > 0
 
 
-def get_all_mentioned_users(message):
-    # message.raw_mentions is a list of user ids.
-    # If no users were directly mentioned, then it returns an empty list
-    # We use a set to prevent duplicates
+def get_all_mentioned_users(message: discord.Message):
+    """
+
+    Returns a set of all user ids mentioned in a message
+    :param message: discord.Message - The message with mentions
+    :return: set
+    """
     mentioned_users = set()
+
+    everyone = contains_everyone(message.content)
+    here = contains_here(message.content)
 
     # Mentioning everyone makes every other mention obsolete, so it's
     # split into its own case. All other mentions can be combined
-    if contains_everyone(message.content):
+    if everyone:
         # Add all users in the server
         mentioned_users.update({member.id for member in
                                message.channel.members})
+    elif not message.mentions and not message.role_mentions and not here:
+        # No mentions, add only the caller
+        mentioned_users.add(message.author.id)
     else:
-        here = contains_here(message.content)
-        if not message.mentions and not message.role_mentions and not here:
-            # No mentions, add only the caller
-            mentioned_users.add(message.author.id)
         if here:
-            # Add all users in the server who are online
+            # Add all users in the server who are not offline
             mentioned_users.update({member.id
                                     for member in message.channel.members
                                     if is_here(member)
                                     })
         if message.role_mentions:
+            # Add all users with the mentioned role
             mentioned_users.update({member.id
                                     for role in message.role_mentions
                                     for member in role.members
                                     })
-        if message.raw_mentions:  # List of user ids, don't need to convert
+        if message.raw_mentions:
+            # Add all directly mentioned users
+            # raw_mentions is a list of user ids
             mentioned_users.update(set(message.raw_mentions))
 
     return mentioned_users
 
 
-def is_here(member):
+def is_here(member: discord.Member):
+    """ Checks if a user would be pinged with @here """
     return str(member.status) != 'offline'
