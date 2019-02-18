8a47148394ae2aa309b083bcaa50801645fd81c8
Hammerlord <nepharus@gmail.com>
2019-02-18 12:30:06 -0800

    PR comments--Use more parametres and rename modules

--- keystones/tests/mocks.py
+++ keystones/tests/mocks.py
@@ -1,22 +1,28 @@
+from typing import List
+
+
 class DiscordUser:
     """
     A mock Discord User object.
     """
-    def __init__(self):
-        self.id = 1
-        self.name = "Discord User"
+    def __init__(self, user_id=1, name="Discord User"):
+        self.id = user_id
+        self.name = name
 
 
 class DiscordMessage:
     """
     A mock Discord Message object.
     """
-    def __init__(self, content: str):
-        self.author = DiscordUser()
+    def __init__(self, content: str, author=DiscordUser()):
         self.content = content
+        self.author = author
 
-        # Not a real Message property: helper to mock the 'args' parametre
-        self.args = self.content.split(' ')[1:]
+    def get_args(self) -> List[str]:
+        """
+        Helper to mock the 'args' parametre. Not a real Message property.
+        """
+        return self.content.split(' ')[1:]
 
 
 class DiscordCtx:
