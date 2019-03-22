13ed7387efb704d66dae3243134deaccb1f67575
Hovsep <hovseplalikian@gmail.com>
2019-03-21 18:01:34 -0700

    Add test for sanitize

--- keystones/test/test_sanitize.py
+++ keystones/test/test_sanitize.py
@@ -3,13 +3,23 @@ import unittest
 from keystones.core.command_validation import sanitize
 
 
-class SanitizeTests(unittest.TestCase):
+class TestSanitize(unittest.TestCase):
     def test_remove_backticks(self):
-        user_input = "Trololo `lo` lol"
+        user_input = 'Trololo `lo` lol'
         output = sanitize(user_input)
-        self.assertEqual(output, "Trololo lo lol")
+        self.assertEqual(output, 'Trololo lo lol')
 
     def test_remove_linebreaks(self):
-        user_input = "Tralala \nla\n la"
+        user_input = 'Tralala \nla\n la'
         output = sanitize(user_input)
-        self.assertEqual(output, "Tralala la la")
+        self.assertEqual(output, 'Tralala la la')
+
+    def test_remove_backticks_and_linebreaks(self):
+        user_input = 'Tralala \nla `la`'
+        output = sanitize(user_input)
+        self.assertEqual(output, 'Tralala la la')
+
+    def test_normal(self):
+        user_input = 'Normal text'
+        output = sanitize(user_input)
+        self.assertEqual(output, 'Normal text')
