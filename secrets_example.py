78b976948b10aacf87b9715ab959b4c0f10d34dc
Hovsep <hovseplalikian@gmail.com>
2020-06-06 16:16:14 -0700

    Add an example secrets file

---
+++ secrets_example.py
@@ -0,0 +1,11 @@
+# This file serves as an example for what's needed in your secrets.py file.
+# You can make a copy of this and rename it secrets.py.
+
+
+# Create an application at https://discord.com/developers/applications
+# Then go to the `bot` section of your app and see the token.
+DISCORD_TOKEN = 'YOUR TOKEN HERE'
+
+# Create a client at https://develop.battle.net/access/clients
+BLIZZARD_CLIENT_ID = 'YOUR TOKEN HERE'
+BLIZZARD_CLIENT_SECRET = 'YOUR TOKEN HERE'
